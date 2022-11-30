5073f3c2e87f2e0d367a853741a04d48a89f9026
Johannes Berthel <mail@johannesberthel.de>
2022-11-30 10:54:31 +0000

    track time simultaneously inside clickup

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -30,6 +30,7 @@ import Header from "./shared/Header"
 import { head } from "lodash"
 import TimeInputParser from "utils/TimeInputParser"
 import { get } from "lodash/fp"
+import { Clickup } from "../utils/clickup"
 
 @observer
 class App extends Component {
@@ -133,6 +134,14 @@ class App extends Component {
   handleSubmit = (event) => {
     event.preventDefault()
     const { service } = this.state
+
+    if (this.changesetWithDefaults.remote_service == 'clickup') {
+      (new Clickup()).track(
+        this.changesetWithDefaults.remote_id,
+        this.changesetWithDefaults.seconds,
+        this.changesetWithDefaults.billable
+      )
+    }
 
     sendMessage(
       "createActivity",
